2baf37c19dd237e382edd478f34e6cdb9b68bc4b
Hoffman <cjhoffman@serv16.divms.uiowa.edu>
2017-04-05 17:54:25 -0500

    Updated webserver that works, no multiplexing

--- webserver.py
+++ webserver.py
@@ -1,25 +1,30 @@
-import socket
+import socket, codecs 
 
 Host = '127.0.0.1'
-Port = '8000'
+Port = 8000
 
 listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listener.bind((Host, Port))
-listener.listen(1)
+listener.listen(5)
 print('Serving HTTP on port %s ...' % Port)
 
 while True:
 	client, clientAddr = listener.accept();
 	request = client.recv(1024)
 	print(request)
+        
+        httpResponse = "This Works" 
 
-	httpResponse = """\
-	HTTP/1.1 200 OK
-
-	Hello, World!
-	"""
 
 	client.sendall(httpResponse)
 	client.close()
+
+
+
+def process_http_header(socket):
+    print('Implement HTTP Header Processing')
+
+
+
 
